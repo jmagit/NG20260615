096c1d4b3cdb45a77cc8b82e1abd99485d769be1
Javier <35864946+jmagit@users.noreply.github.com>
2026-06-16 13:28:13 +0200

    NotificationService

--- curso/src/app/ejemplos/demos/demos.ts
+++ curso/src/app/ejemplos/demos/demos.ts
@@ -2,24 +2,53 @@
 import { JsonPipe } from '@angular/common';
 import { Component, computed, effect, inject, resource, signal } from '@angular/core';
 import { LoggerService } from '@my-library';
+import { Unsubscribable } from 'rxjs';
+import { NotificationService, NotificationType } from 'src/app/common-services';
+import { Notification } from "src/app/layout";
 
 @Component({
   selector: 'app-demos',
-  imports: [ JsonPipe ],
+  imports: [JsonPipe, Notification],
   templateUrl: './demos.html',
   styleUrl: './demos.css',
+  // providers: [ NotificationService ]
 })
 export class Demos {
-  private readonly logger: LoggerService
+  private readonly logger= inject(LoggerService)
 
+  // Ejemplo de servicios
+  constructor(public vm: NotificationService) {
+    // effect(() => {
+    //   if (this.vm.HayNotificaciones() && this.vm.Listado()[this.vm.Listado().length - 1].Type === NotificationType.error) {
+    //     window.alert(`Efecto: ${this.vm.Listado()[this.vm.Listado().length - 1].Message}`);
+    //     this.vm.remove(this.vm.Listado().length - 1);
+    //   }
+    // })
+   }
+
+  // private suscriptor: Unsubscribable | undefined;
+  // ngOnInit(): void {
+  //   this.suscriptor = this.vm.Notificacion.subscribe(n => {
+  //     if (n.Type !== NotificationType.error) { return; }
+  //     window.alert(`Suscripción: ${n.Message}`);
+  //     this.vm.remove(this.vm.Listado().length - 1);
+  //   });
+  // }
+  // ngOnDestroy(): void {
+  //   if (this.suscriptor) {
+  //     this.suscriptor.unsubscribe();
+  //   }
+  // }
+
+  // ejemplo de servicios
   // constructor(private logger: LoggerService) {
-  constructor() {
-    this.logger = inject(LoggerService)
-    this.logger.error('es un error')
-    this.logger.warn('es un warn')
-    this.logger.info('es un info')
-    this.logger.log('es un log')
-  }
+  // constructor() {
+  //   this.logger = inject(LoggerService)
+  //   this.logger.error('es un error')
+  //   this.logger.warn('es un warn')
+  //   this.logger.info('es un info')
+  //   this.logger.log('es un log')
+  // }
 
   // ejemplo de señales
   // readonly conSignal = signal(0)
